67cc429fa9872b92587bd1bfe1790d71a74cde3f
Abeeha Fatima <saytoabeeha@gmail.com>
2026-07-21 00:56:09 +0500

    Implement temperature conversion from Celsius

---
+++ 2469-convert-the-temperature.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = celsius + 273.15
+        farenheit = celsius  * 1.80 + 32.00
+        
+        result = []
+        result.append(kelvin)
+        result.append(farenheit)
+        return result  
